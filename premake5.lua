287f9931a6324c06852732a5e1f4d4ed31c19929
João Lobo <30907944+joaodiaslobo@users.noreply.github.com>
2020-08-16 15:29:10 +0100

    Project Generation System with Premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Newton"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Newton"
+	location "Newton"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"NEWTON_PLATFORM_WINDOWS",
+			"NEWTON_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "NEWTON_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "NEWTON_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "NEWTON_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Newton/vendor/spdlog/include",
+		"Newton/src"
+	}
+
+	links
+	{
+		"Newton"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"NEWTON_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "NEWTON_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "NEWTON_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "NEWTON_DIST"
+		optimize "On"
